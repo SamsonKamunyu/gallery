77d9ae851c1a8a53bacbbcc295e9bbf958005a5d
SamsonKamunyu <kamunyunjoroge7@gmail.com>
2024-04-21 22:12:19 +0300

    adding Jenkinsfile updates

--- _config.js
+++ _config.js
@@ -2,8 +2,11 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://samsonkamunyu:@Brian#2001@project.s2tkr35.mongodb.net/?retryWrites=true&w=majority&appName=Project',
-    development: 'mongodb+srv://samsonkamunyu:@Brian#2001@project.s2tkr35.mongodb.net/?retryWrites=true&w=majority&appName=Project',
-    test: 'mongodb+srv://samsonkamunyu:@Brian#2001@project.s2tkr35.mongodb.net/?retryWrites=true&w=majority&appName=Project',
+    production: 'mongodb+srv://samsonkamunyu:IzxAWkrf9XOROU8M@project.s2tkr35.mongodb.net/?retryWrites=true&w=majority&appName=Project',
+    development: 'mongodb+srv://samsonkamunyu:IzxAWkrf9XOROU8M@project.s2tkr35.mongodb.net/?retryWrites=true&w=majority&appName=Project',
+    test: 'mongodb+srv://samsonkamunyu:IzxAWkrf9XOROU8M@project.s2tkr35.mongodb.net/?retryWrites=true&w=majority&appName=Project',
 }
 module.exports = config;
+
+
+// IzxAWkrf9XOROU8M
